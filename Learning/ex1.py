40260ce0a260fb7d4c14a69c4eb0293381d8ef2f
hsmalley <hugh@thehijax.com>
2014-06-03 19:47:24 -0400

    Ex 1 - 3

--- Learning/ex1.py
+++ Learning/ex1.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-.
+
 print "Hello"
 print "Holy CCCCCCRAP"
 print "IT'S PYTHON TIME!"
---
+++ Learning/ex3.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-.
+
+# Time to count some chickens!
+print "Hens", 25 + 30 / 6
+print "Cocks", 100 - 25 * 3 % 4
+
+# Now to count some eggs!
+print "It's egg count'n time!"
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+#Now for something else
+print "Is it true that 3 + 2 < 5 - 7"
+print 3 + 2 < 5 - 7
+
+print "What is 3 + 2", 3 + 2
+print "What is 5 - 7", 5 - 7
+
+print "Oh that's why it's false"
+print "Let's do some more"
+
+print "Is it greater", 5 > -2
+print "Is it greater than or equal", 5 >= -2
+print "Is it less or equal?", 5 <+ -2
+
+#Done!
