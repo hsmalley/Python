8c03df4bb30ee02491108590bed3bb8b4ca4554e
hsmalley <hugh@thehijax.com>
2014-06-03 20:42:24 -0400

    Renames

---
+++ Learning/ex01.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+print "Hello"
+print "Holy CCCCCCRAP"
+print "IT'S PYTHON TIME!"
---
+++ Learning/ex02.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+# COMMENTS!!!
+# AWW YISS!
+
+print "And here is a trailing comment" #comment is here!
